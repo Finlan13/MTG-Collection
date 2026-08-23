388998fc02afe5048be2cf94bafb05754e2c4e47
Finlan13 <j.wilson060.216@gmail.com>
2026-08-24 09:48:04 +1000

    Create initial collection page

---
+++ website/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <main>
+      <h1>MTG Collection</h1>
+
+      <p>
+        My Magic: The Gathering collection
+      </p>
+    </main>
+  );
+}
+
+export default App;
